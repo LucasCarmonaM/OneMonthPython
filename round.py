f5358ffca045705e2c1d54f41446f24531e10b25
lucasACM <lucas.carmonam@utem.cl>
2020-09-03 16:32:38 -0400

    init semana 2

--- round.py
+++
@@ -1,5 +0,0 @@
-# Redondeo python
-float_ = 3.1416
-print(f"Redondeado al segundo decimal: {float_:.2f}")
-print(f"Redondeado al tercer decimal: {float_:.3f}")
-print(f"Redondeado a la unidad: {float_:.0f}")
